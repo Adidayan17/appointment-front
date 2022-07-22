dd77d689d76eaf5e7560433a9d326218358d289b
adidayan17 <Adidayan17@gmail.com>
2022-07-22 16:58:40 +0300

    second commit

---
+++ src/sharedComponents/CustomBoxs.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import { styled } from '@mui/system';
+
+export const HBox = styled(Box)`
+  flex-direction:row;
+  display:flex;
+`;
+
+export const VBox = styled(Box)`
+    flex-direction:column;
+    display:flex;
+`;
